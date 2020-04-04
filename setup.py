3316219f9fff6803d66aee29eaffa691f0dcd912
zclaytor <zclaytor@hawaii.edu>
2020-04-03 17:01:28 -1000

    incorporate mcmc function as method

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kiauhoku",
-    version="1.0.1",
+    version="1.0.2",
     author="Zachary R. Claytor",
     author_email="zclaytor@hawaii.edu",
     description="utilities for interacting and interpolating stellar evolution models",
