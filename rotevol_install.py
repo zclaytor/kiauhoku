d8850a8286a777c1c1e093fb4ef36429dd21e457
zclaytor <zclaytor@hawaii.edu>
2020-05-06 13:46:30 -1000

    remove unused function _to_string

--- rotevol_install.py
+++ rotevol_install.py
@@ -31,19 +31,6 @@ eep_params = dict(
                  150], # TAMS-RGBump
 )
 
-def _to_string(val):
-    '''
-    Converts a given float (`val`) of mass, metallicity, or alpha enhancement
-    to a string (`my_str`) formatted for the model filename. For example, a
-    metallicity [M/H] = -0.5 corresponds to the string '-050', and the mass
-    1.32 corresponds to the string '132'.
-    '''
-    if val < 0:
-        my_str = '-'
-    else:
-        my_str = ''
-    my_str += f'{abs(100*val):03.0f}'
-    return my_str
 
 def from_rotevol(path):
     fname = path.split('/')[-1]
