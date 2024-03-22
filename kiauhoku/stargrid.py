319ba33a98727965d29e50ab58f31f420b9b5874
zclaytor <zclaytor@hawaii.edu>
2024-03-22 11:32:58 -0400

    Revert "allow get_star_eep to return exact match"
    This reverts commit 0825222a577b410ede3b96bf5a63a9280dd132ae.
    
    The update made fit_star fail, because it uses a numpy array to get several models at a time.

--- kiauhoku/stargrid.py
+++ kiauhoku/stargrid.py
@@ -289,18 +289,6 @@ class StarGridInterpolator(DFInterpolator):
         data = self.grid[idx]
         return data
     
-    def _get_star_eep(self, index):
-        '''
-        Return an EEP from the grid if there is an exact match,
-        rather than interpolating.
-
-        Note that this assumes `index in self.index_names` is True.
-        '''
-        names = self.index_names
-        idx = tuple([self.idxwhere(l, i) for l, i in zip(names, index)])
-        data = self.grid[idx]
-        return data
-    
     def get_primary_eeps(self):
         '''Return indices of Primary EEPs in the EEP-based tracks.
         '''
@@ -322,11 +310,7 @@ class StarGridInterpolator(DFInterpolator):
         >>> star = grid.get_star_eep((0.987, 0.2, 350))
         '''
 
-        if index in self.index:
-            star_values = self._get_star_eep(index)
-        else:
-            star_values = self(index)
-            
+        star_values = self(index)
         if len(np.shape(index)) == 1:
             star = pd.Series(star_values, index=self.columns)
         else:
