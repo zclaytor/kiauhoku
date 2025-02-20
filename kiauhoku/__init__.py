0c03ef1a0b1e0281030f4d88a72ad7f4dfa028b0
Zach Claytor <zclaytor@stsci.edu>
2025-02-20 16:19:58 -0500

    bring StarGrid to highest level namespace

--- kiauhoku/__init__.py
+++ kiauhoku/__init__.py
@@ -1,5 +1,6 @@
 name = "kiauhoku"
 
+from .stargrid import StarGrid, StarGridInterpolator
 from .stargrid import install_grid, load_interpolator, download
 from .stargrid import load_grid, load_full_grid, load_eep_grid
 from .calc_HZ import add_HZ, add_HZ_custom
