6994b66b42038eb5f580df88ad8848a6167f645a
Zach Claytor <zclaytor@stsci.edu>
2025-02-21 12:05:53 -0500

    make calc_HZ a subpackage

--- kiauhoku/__init__.py
+++ kiauhoku/__init__.py
@@ -3,6 +3,5 @@ name = "kiauhoku"
 from .stargrid import StarGrid, StarGridInterpolator
 from .stargrid import install_grid, load_interpolator, download
 from .stargrid import load_grid, load_full_grid, load_eep_grid
-from .calc_HZ import add_HZ, add_HZ_custom
 
 __version__ = "2.0.0"
