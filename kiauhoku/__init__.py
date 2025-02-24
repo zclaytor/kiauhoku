1c7d4a151f65c227fc66ea5061557ecd6faf385f
Zach Claytor <zclaytor@stsci.edu>
2025-02-24 13:35:18 -0500

    bump version

--- kiauhoku/__init__.py
+++ kiauhoku/__init__.py
@@ -4,4 +4,4 @@ from .stargrid import StarGrid, StarGridInterpolator
 from .stargrid import install_grid, load_interpolator, download
 from .stargrid import load_grid, load_full_grid, load_eep_grid
 
-__version__ = "2.0.0"
+__version__ = "2.1.0-dev3"
